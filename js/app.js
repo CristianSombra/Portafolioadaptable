e7fb4c2eaadf7831bd34f8ded35308962561a8ff
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-04-04 20:48:29 -0300

    Actualización

--- js/app.js
+++ js/app.js
@@ -7,3 +7,36 @@ imagenes.forEach((imagen) => {
 		imagenModal.src = ruta;
 	});
 });
+
+
+/* Scroll to Top */
+
+const handleScroll = () => {
+    const scrollY = window.scrollY;
+    const scrollToTopBtn = document.getElementById("scroll-to-top");
+
+    if (scrollY > 300) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+
+window.addEventListener("scroll", handleScroll);
+
+const ScrollTop = () => {
+    return `
+        <button id="scroll-to-top" class="btn" onclick="scrollToTop()">
+			<i class="fas fa-arrow-up"></i>
+        </button>
+    `;
+};
+
+document.body.insertAdjacentHTML("beforeend", ScrollTop());
